2c39bdf7c19663cd91e0489daaf72de8cf40a020
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-22 16:08:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-﻿var app = angular.module('employeePortal', []);
-
-app.controller('EmployeeController', ['$scope', function ($scope) {
-    console.log('Controller initialized');
-
-    $scope.employees = [];
-    $scope.employee = {};
-    $scope.testMessage = "Hello, World!";
-
-    $scope.addEmployee = function () {
-        console.log('Add Employee function called');
-        if ($scope.employee.name && $scope.employee.email) {
-            console.log('Adding Employee:', $scope.employee);
-            $scope.employees.push(angular.copy($scope.employee));
-            console.log('Employees List:', $scope.employees);
-            $scope.employee = {};
+angular.module('LunchChecker', [])
+.controller('LunchCheckController', ['$scope', function($scope) {
+    $scope.checkLunch = function() {
+        if (!$scope.lunchMenu) {
+            $scope.message = "Please enter data first";
+        } else {
+            var items = $scope.lunchMenu.split(',').filter(item => item.trim() !== '');
+            $scope.message = items.length <= 3 ? "Enjoy!" : "Too much!";
         }
-    };
-
-    $scope.removeEmployee = function (index) {
-        console.log('Remove Employee function called');
-        $scope.employees.splice(index, 1);
-    };
-
-    $scope.changeMessage = function () {
-        console.log('Change Message function called');
-        $scope.testMessage = "Button clicked!";
     };
 }]);
