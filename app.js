fdcba0b30f370aaa48571c32e2835e458aa42b36
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-22 15:57:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('employeePortal', []);
+
+app.controller('EmployeeController', ['$scope', function ($scope) {
+    console.log('Controller initialized');
+
+    $scope.employees = [];
+    $scope.employee = {};
+    $scope.testMessage = "Hello, World!";
+
+    $scope.addEmployee = function () {
+        console.log('Add Employee function called');
+        if ($scope.employee.name && $scope.employee.email) {
+            console.log('Adding Employee:', $scope.employee);
+            $scope.employees.push(angular.copy($scope.employee));
+            console.log('Employees List:', $scope.employees);
+            $scope.employee = {};
+        }
+    };
+
+    $scope.removeEmployee = function (index) {
+        console.log('Remove Employee function called');
+        $scope.employees.splice(index, 1);
+    };
+
+    $scope.changeMessage = function () {
+        console.log('Change Message function called');
+        $scope.testMessage = "Button clicked!";
+    };
+}]);
