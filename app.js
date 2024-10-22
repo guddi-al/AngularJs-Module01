0fb5b4dd7268bada3e9517206fb38fd10e48fab6
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-22 16:11:02 +0530

    app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ angular.module('LunchChecker', [])
         if (!$scope.lunchMenu) {
             $scope.message = "Please enter data first";
         } else {
-            var items = $scope.lunchMenu.split(',').filter(item => item.trim() !== '');
+            var items = $scope.lunchMenu.split(',').map(item => item.trim()).filter(item => item !== '');
             $scope.message = items.length <= 3 ? "Enjoy!" : "Too much!";
         }
     };
 }]);
+
